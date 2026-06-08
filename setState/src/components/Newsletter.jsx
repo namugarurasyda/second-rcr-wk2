46820932492b548678844bf427ec38d1951f86dd
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-09 00:43:10 +0300

    adding th onclick event on the Newsletter

--- setState/src/components/Newsletter.jsx
+++ setState/src/components/Newsletter.jsx
@@ -1,10 +1,13 @@
 import { ActionButton } from "./ActionButton";
 
 export const Newsletter = () => {
+    const handleSubscribe = () => {
+        alert("Thank you for subscribing to our newsletter!");
+    }
   return (
     <div>
       <h2>Subscribe to Newsletter</h2>
-      <ActionButton text="Subscribe" />
+      <ActionButton text="Subscribe" onClick={handleSubscribe} />
     </div>
   );
 };
