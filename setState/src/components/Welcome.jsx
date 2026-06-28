c9c14d5cb0074f92ca4c956fdaed0759afca9ce7
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-28 23:52:45 +0300

    Changes

--- setState/src/components/Welcome.jsx
+++ setState/src/components/Welcome.jsx
@@ -1,11 +1,11 @@
 // class component
 import React, { Component } from 'react'
+import { useState } from 'react';
 
-class Welcome extends Component{
-    render(){
-        return <h1> Hello RCR week Welcome component</h1>
-
-    }
+const Welcome = () => {
+    const [message, setMessage] = useState("Hello RCR week Welcome component");
+    
+    return <h1>{message}</h1>;
 }
 
 export default Welcome
