0d8926aac06475bf2d14b809459a48db0efc6742
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-28 23:53:53 +0300

    Adding a useffect hook

--- setState/src/components/Welcome.jsx
+++ setState/src/components/Welcome.jsx
@@ -1,6 +1,6 @@
 // class component
 import React, { Component } from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Welcome = () => {
     const [message, setMessage] = useState("Hello RCR week Welcome component");
